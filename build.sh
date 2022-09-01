1fbe11207b81a4d74077e90b25b56ff00793cebd
Fede Calendino <fede@calendino.com>
2022-09-01 09:51:27 +0200

    Migrate to nuitka

--- build.sh
+++ build.sh
@@ -1,12 +1,20 @@
 function build() {
-  poetry run nuitka --onefile "src/$1.py"
+  echo "Generating binary file for $1"
+  echo
 
-  rm -rf "$1.build/"
-	rm -rf "$1.dist/"
-	rm -rf "$1.onefile-build/"
+  poetry run python3 -m nuitka --onefile "src/$1.py"
 
-	mkdir "./bin"
-	mv "$1.bin" "./bin/$1"
+  echo
+  echo "Cleaning up nuitka temporary files..."
+   rm -rf "$1.build/"
+   rm -rf "$1.dist/"
+   rm -rf "$1.onefile-build/"
+
+	 mkdir bin 2> /dev/null
+	 mv "$1.bin" "./bin/$1"
+
+  echo
+	echo "Finished generating bin file for $1"
 }
 
 build main
