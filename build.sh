302fb7cebaed205a3bd2613cc4c22335ddd6a10c
Fede Calendino <fede@calendino.com>
2022-09-01 09:43:40 +0200

    Migrate to nuitka

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
-poetry run pyinstaller --onefile --paths ./src/ src/main.py
+function build() {
+  poetry run nuitka --onefile "src/$1.py"
 
-rm -rf ./build
-rm *.spec
+  rm -rf "$1.build/"
+	rm -rf "$1.dist/"
+	rm -rf "$1.onefile-build/"
+
+	mkdir "./bin"
+	mv "$1.bin" "./bin/$1"
+}
+
+build main
